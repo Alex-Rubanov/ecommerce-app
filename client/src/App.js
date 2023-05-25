677e95b4e47166d18ffd16435def2941ad11f4d9
Alex <rubanovv.alex@gmail.com>
2023-05-25 15:14:45 +0200

    added Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './scenes/home/Home'
 import ItemDetails from './scenes/itemDetails/ItemDetails'
 import Confirmation from './scenes/checkout/Confirmation'
 import Checkout from './scenes/checkout/Checkout'
+import Navbar from './scenes/global/Navbar'
 
 const ScrollToTop = () => {
   const { pathname } = useLocation()
@@ -19,6 +20,7 @@ const App = () => {
   return (
     <div className='app'>
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
--- client/src/scenes/global/Navbar.js
+++ client/src/scenes/global/Navbar.js
@@ -1,5 +1,71 @@
+import { Badge, Box, IconButton } from '@mui/material'
+import {
+  PersonOutlined,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined
+} from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
+import { shades } from '../../theme'
+
 const Navbar = () => {
-  return <>Navbar</>
+  const navigate = useNavigate()
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        height: '60px',
+        backgroundColor: 'rgba(255, 255, 255, 0.95)',
+        color: 'black',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        zIndex: '1'
+      }}
+    >
+      <Box
+        sx={{
+          width: '80%',
+          margin: 'auto',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <Box
+          onClick={() => navigate('/')}
+          sx={{
+            '&:hover': { cursor: 'pointer' }
+          }}
+          color={shades.secondary[600]}
+        >
+          LuxeMart
+        </Box>
+        <Box
+          display='flex'
+          justifyContent='space-between'
+          columnGap='20px'
+          zIndex='2'
+        >
+          <IconButton sx={{ color: shades.primary[900] }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: shades.primary[900] }}>
+            <PersonOutlined />
+          </IconButton>
+          <IconButton sx={{ color: shades.primary[900] }}>
+            <ShoppingBagOutlined />
+          </IconButton>
+          <IconButton sx={{ color: shades.primary[900] }}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default Navbar
