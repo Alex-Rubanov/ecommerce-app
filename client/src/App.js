bff130db6674588614b314ae716835a9781436eb
Alex <rubanovv.alex@gmail.com>
2023-05-25 02:50:08 +0200

    added project routes structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
-function App () {
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import Home from './scenes/home/Home'
+import ItemDetails from './scenes/itemDetails/ItemDetails'
+import Confirmation from './scenes/checkout/Confirmation'
+import Checkout from './scenes/checkout/Checkout'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
+const App = () => {
   return (
     <div className='app'>
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='item/:itemId' element={<ItemDetails />} />
+          <Route path='checkout' element={<Checkout />} />
+          <Route path='checkout/success' element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
