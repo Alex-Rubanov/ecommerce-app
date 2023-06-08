25ad3df44b239e084f9d048a873b70516c5c5421
Alex <rubanovv.alex@gmail.com>
2023-06-08 19:36:51 +0200

    started working on Checkout page using Formik/yup libraries and inplementing some basic validation and layout for 2 step form submission

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,5 +1,50 @@
+import { useSelector } from 'react-redux'
+import { Box, Button, Stepper, Step, StepLabel } from '@mui/material'
+import { Formik } from 'formik'
+import { useState } from 'react'
+import * as yup from 'yup'
+import { shades } from '../../theme'
+import { initialValues, checkoutSchema } from '../../utils/checkoutValidation'
+
 const Checkout = () => {
-  return <>Checkout</>
+  const [activeStep, setActiveStep] = useState(0)
+  const cart = useSelector(state => state.cart.cart)
+  const isFirstStep = Boolean(activeStep)
+  const isSecondStep = Boolean(activeStep)
+
+  const handleSubmit = async (value, actions) => {
+    setActiveStep(prev => prev + 1)
+  }
+
+  return (
+    <Box width='80%' m='100px auto'>
+      <Stepper activeStep={activeStep} sx={{ m: '20px  0' }}>
+        <Step>
+          <StepLabel>Billing</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Payment</StepLabel>
+        </Step>
+      </Stepper>
+      <Box>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          validationSchema={() => checkoutSchema(yup)[activeStep]}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            setFieldValue
+          }) => <form onSubmit={handleSubmit}></form>}
+        </Formik>
+      </Box>
+    </Box>
+  )
 }
 
 export default Checkout
